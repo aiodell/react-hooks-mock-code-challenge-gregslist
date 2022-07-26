5c30c92b44276810c82d37e9f1ce87e69c23475e
Andrianna <andrianna.odell@gmail.com>
2022-07-26 10:01:21 -0400

    displayed images

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,29 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer(props) {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+    .then((resp) => resp.json())
+    .then((data) => setList(data));
+  }, []);
+
   return (
     <main>
       <ul className="cards">
         {/* use the ListingCard component to display listings */}
+        {list.map((lists) => {
+          return(
+            <ListingCard
+            key= {lists.id}
+            description = {lists.description}
+            image = {lists.image}
+            location = {lists.location}/>
+          )
+        })}
+
       </ul>
     </main>
   );
